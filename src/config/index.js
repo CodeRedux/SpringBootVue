ba378289743f5e26c4290d7ef8ef16b054d1331f
dotProgram <79394384+dotProgram@users.noreply.github.com>
2025-11-21 12:36:23 +0530

    here

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseURL: 'https://0f5ccb447939.ngrok-free.app/', // Your URL here
+    baseURL: 'https://c8402e95ddc7.ngrok-free.app/', // Your URL here
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
